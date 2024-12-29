978bea625a57f306c0b31599b8234a0e5437d2f0
Amit <amit.yadav1.contractor@pepsico.com>
2024-12-29 21:39:58 +0530

    Bucket name correction

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "terraform-state-bucket"
+    bucket  = "terraform-state-bucket12"
     prefix  = "terraform/state"
   }
 }
--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "terraform_state" {
-  name          = "terraform-state-bucket"
+  name          = "terraform-state-bucket12"
   location      = var.region
   force_destroy = true
 }
