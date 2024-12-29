4ab51e1774d697894e0c72cfebd6ca24bfc590cb
Amit <amit.yadav1.contractor@pepsico.com>
2024-12-29 21:54:49 +0530

    File

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "terraform_state" {
-  name          = "terraform-state-bucket12"
+  name          = "terraform-state-bucket"
   location      = var.region
   force_destroy = true
 }
